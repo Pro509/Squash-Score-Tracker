60273540421c30c37dea55bb4c17937793969866
Pritish Dugar <pritishdugar509@gmail.com>
2021-06-14 01:26:58 +0700

    button text fixes

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const player1 = {
     score: 0,
-    buttonDisp: document.querySelector('#p1ButtonDisp'),
     button: document.querySelector('#p1Button'),
     display: document.querySelector('#p1Display')
 }
 
 const player2 = {
     score: 0,
-    buttonDisp: document.querySelector('#p2ButtonDisp'),
     button: document.querySelector('#p2Button'),
     display: document.querySelector('#p2Display')
 }
@@ -27,8 +25,8 @@ form.addEventListener('submit', function(el) {
     el.preventDefault();
     const player1Input = form.elements.player1Name.value;
     const player2Input = form.elements.player2Name.value;
-    player1.buttonDisp.innerText = player1Input;
-    player2.buttonDisp.textContent = player2Input;
+    player1.button.textContent = `+1 ${player1Input}`;
+    player2.button.textContent = `+1 ${player2Input}`;
     shutOverlay();
     // prevent page redirection and more, no default behaviour
 })
